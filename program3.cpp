c974879fa74e6075db70e8e85d4e8d822b0fdfd1
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2021-11-02 19:26:04 +0530

    Add files via upload

---
+++ program3.cpp
@@ -0,0 +1,18 @@
+
+// addition of two  number   accept form user
+
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+   int x=0, y=0;
+   int z=0;
+   cout<<"enter first number";
+   cin>> x;
+   cout<<"enter second numebr";
+   cin>>y;
+    z = x + y;
+	cout<< z;
+	return 0;
+	}
+	
